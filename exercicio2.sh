f80d1baf0398418739365fa02868dd9a6d1bccf5
vinic <andrevmoraes@outlook.com>
2020-10-21 10:58:29 -0300

    att

--- exercicio2.sh
+++ exercicio2.sh
@@ -1,5 +1,7 @@
+#!/usr/bin/env bash
+
 echo "==== For 1"
 for (( i = 0; i < 11; i++)); do
     echo $i
-    [ "$i" % 2 ] && echo "É divisível por 2"
+    [ $(("$i" % 2)) -eq 0 ] && echo "$i é divisível por 2"
 done
